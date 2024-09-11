bec2693f3b9f2711a46d4d6e390584eff3a9da2a
Qendresa Hoti <qendresahhoti@gmail.com>
2024-09-11 17:48:21 +0200

    fixes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-// export { default as Header } from "./Header";
-// export { default as Input } from "./Input";
-// export { default as Layout } from "./Layout";
-// export { default as Footer } from "./Footer";
-// export { default as Card } from "./Card";
-// export { default as Button } from "./Button";
+export { default as Header } from "./Header";
+export { default as Input } from "./Input";
+export { default as Layout } from "./Layout";
+export { default as Footer } from "./Footer";
+export { default as Card } from "./Card";
+export { default as Button } from "./Button";
